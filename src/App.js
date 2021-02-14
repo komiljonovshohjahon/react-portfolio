2518270b52f7f32d3258fbc21155a6bf3ee14174
Shokhjakhon Komiljonov <komiljonovshohjahon@gmail.com>
2021-02-14 16:56:35 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="/navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+              <a class="nav-link" href="/">
+                Features
+              </a>
+              <a class="nav-link" href="/">
+                Pricing
+              </a>
+              <a
+                class="nav-link disabled"
+                href="/"
+                tabindex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
